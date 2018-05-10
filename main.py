e98863d9633c68814857825919375437d6a6db3a
Zhiyan Foo <zhiyanfoo@gmail.com>
2018-05-10 16:01:00 +0700

    import rename

--- main.py
+++ main.py
@@ -1,24 +1,22 @@
 import webapp2
 from webapp2 import Route
 
-from handlers import (MainPage, Login, SignUp, AddPost, Logout, UserPosts,
-                      SinglePost, EditPost, DeletePost, ToggleLike, AddComment,
-                      DeleteComment, EditComment)
+import handlers as h
 
 app = webapp2.WSGIApplication([
-    ('/', MainPage),
-    ('/login', Login),
-    ('/signup', SignUp),
-    ('/logout', Logout),
-    ('/add_post', AddPost),
-    Route('/b/<name:\w+>', UserPosts),
-    Route(r'/b/<name:\w+>/<id:\d+>', SinglePost),
-    Route(r'/b/<name:\w+>/<id:\d+>/edit', EditPost),
-    Route(r'/b/<name:\w+>/<id:\d+>/delete', DeletePost),
-    Route(r'/b/<name:\w+>/<id:\d+>/togglelike', ToggleLike),
-    Route(r'/b/<name:\w+>/<id:\d+>/addcomment', AddComment),
+    ('/', h.MainPage),
+    ('/login', h.Login),
+    ('/signup', h.SignUp),
+    ('/logout', h.Logout),
+    ('/add_post', h.AddPost),
+    Route('/b/<name:\w+>', h.UserPosts),
+    Route(r'/b/<name:\w+>/<id:\d+>', h.SinglePost),
+    Route(r'/b/<name:\w+>/<id:\d+>/edit', h.EditPost),
+    Route(r'/b/<name:\w+>/<id:\d+>/delete', h.DeletePost),
+    Route(r'/b/<name:\w+>/<id:\d+>/togglelike', h.ToggleLike),
+    Route(r'/b/<name:\w+>/<id:\d+>/addcomment', h.AddComment),
     Route(r'/b/<name:\w+>/<id:\d+>/<c_user_name:\d+>/<c_id:\d+>/delete',
-          DeleteComment),
+          h.DeleteComment),
     Route(r'/b/<name:\w+>/<id:\d+>/<c_user_name:\d+>/<c_id:\d+>/edit',
-          EditComment),
+          h.EditComment),
 ], debug=True)
